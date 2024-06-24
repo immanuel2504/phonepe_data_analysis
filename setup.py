d68c0eaa3c440e50dcf8f771ced71d2468d04690
manova <manova.abraham@gmail.com>
2024-06-24 13:48:17 +0530

    Phonepay_project

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+def read_requirements():
+    with open('requirements.txt') as req:
+        return req.read().splitlines()
+
+setup(
+    name='Phonepe_data_analysis',
+    version='0.1',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=read_requirements(),
+    author='Immanuel',  # Replace with your name
+    author_email='immanuel2504@gmail.com',
+    description='A project for analyzing PhonePe transaction data',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/phonepe_data_analysis',  # Replace with your GitHub repo URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
